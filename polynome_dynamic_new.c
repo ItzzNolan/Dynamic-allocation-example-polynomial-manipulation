59bce797d65bcbd0efec1b05b6bad3f88bb39769
ItzzNolan <nolan.02g@gmail.com>
2024-03-29 15:52:51 +0100

    Rearrange Files + ProjectBetaDone

--- polynome_dynamic_new.c
+++ polynome_dynamic_new.c
@@ -44,6 +44,51 @@ void afficher_polynome(Polynome p) {
     printf("\n");
 }
 
+void ajuster_degre(Polynome *p) {
+    int new_degre = p->degre;
+    while (p->poly[new_degre] == 0 && new_degre > 0) {
+        new_degre--;
+    }
+
+    if (new_degre != p->degre) {
+        float *n_poly = malloc((new_degre + 1) * sizeof(float));
+        for (int i = 0; i <= new_degre; i++) {
+            n_poly[i] = p->poly[i];
+        }
+        free(p->poly);
+        p->poly = n_poly;
+        p->degre = new_degre;
+    }
+}
+
+Polynome soustraction_polynome(Polynome p1, Polynome p2) {
+    Polynome result;
+    result.degre = (p1.degre > p2.degre) ? p1.degre : p2.degre; //coeff
+    result.poly = malloc((result.degre + 1) * sizeof(float));
+
+    int isZero = 1;
+
+    for (int i = 0; i <= result.degre; ++i) {
+        float coeff1 = (i <= p1.degre) ? p1.poly[i] : 0;
+        float coeff2 = (i <= p2.degre) ? p2.poly[i] : 0;
+        result.poly[i] = coeff1 - coeff2;
+
+        if (result.poly[i] != 0){
+            isZero = 0;
+        }
+    }
+    if (isZero) {
+        free(result.poly);
+        printf("f(x) = 0\n");
+        result.degre = 0;
+        result.poly = malloc(sizeof(float));
+        result.poly[0] = 0;
+    } else {
+        ajuster_degre(&result);
+    }
+    return result;
+}
+
 
 int main(){
     Polynome p1, p2;
@@ -65,6 +110,17 @@ int main(){
 
     printf("\nPolynome 2 saisi :\n");
     afficher_polynome(p2);
+
+    sleep(3);
+    system("cls");
+
+    Polynome difference = soustraction_polynome(p1, p2);
+    printf("\nPolynome 1 - Polynome 2 :\n");
+    afficher_polynome(difference);
+
+    free(p1.poly);
+    free(p2.poly);
+    free(difference.poly);
 
     return 0;
 }
